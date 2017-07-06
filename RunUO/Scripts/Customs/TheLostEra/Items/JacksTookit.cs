66260e0d19792dc34400a6b084e0b9f43fe0540e
Jacob Harrison <Jacob Harrison>
2017-07-05 20:12:58 -0700

    Adding serialization methods as they are required.

--- RunUO/Scripts/Customs/TheLostEra/Items/JacksTookit.cs
+++ RunUO/Scripts/Customs/TheLostEra/Items/JacksTookit.cs
@@ -1,6 +1,6 @@
 ﻿namespace Server.Items
 {
-	public sealed class JacksToolkit : TinkerTools
+	public class JacksToolkit : TinkerTools
 	{
 		[Constructable]
 		public JacksToolkit() : base( int.MaxValue )
@@ -9,12 +9,30 @@
 			Hue = 1895;
 		}
 
+		public JacksToolkit( Serial serial ) : base( serial )
+		{
+		}
+
 		public override bool BreakOnDepletion { get { return false; } }
 		public override bool ShowUsesRemaining { get { return false; } set {} }
 
 		public override string DefaultName
 		{
 			get { return "Jack's Toolkit"; }
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
 		}
 	}
 }
